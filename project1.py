a30c13164432e4dd7d5c69cfc4766299aa3bbe97
Sameerig <73588390+Sameerig@users.noreply.github.com>
2021-09-03 14:04:02 +0530

    day 1 project

---
+++ project1.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+city =input("What is thee name of the city you grew up in ?\n")
+pet =input("What is your pet's name? \n")
+
+print("Your brand name could be "+ city +" "+pet)
